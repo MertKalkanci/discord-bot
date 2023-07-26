d988532f2d21f3476320f6473ed63814c7f93a98
Mert Kalkancı <63818504+MertKalkanci@users.noreply.github.com>
2023-07-26 16:11:17 +0300

    img generation from command via stable diffisuon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext.commands import Bot
+import diffuser_control
 import os
 
 TOKEN = open("token.txt", "r").read()
@@ -16,5 +17,16 @@ bot = Bot(intents=intents,command_prefix="$")
 @bot.event
 async def on_connect():
   print("Bot Connected")
+
+@bot.command()
+async def generate(ctx, *args):
+    text = ""
+    for i in args:
+        text += i + " "
+    text = text.rstrip(text[-1])
+
+    img = diffuser_control.generate_img(text)
+
+    await ctx.send(content=f'{text}',file=discord.File(img))
 
 bot.run(TOKEN)

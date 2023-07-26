6bf64229787da446de2c557cb753101a3abcea1c
Mert Kalkancı <63818504+MertKalkanci@users.noreply.github.com>
2023-07-26 16:05:23 +0300

    basic bot implementation

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext.commands import Bot
+import os
+
+TOKEN = open("token.txt", "r").read()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.moderation = True
+
+bot = Bot(intents=intents,command_prefix="$")
+
+
+
+@bot.event
+async def on_connect():
+  print("Bot Connected")
+
+bot.run(TOKEN)
